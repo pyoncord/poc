16045257d062b6d616b13dc97be7d673f8ed3604
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2023-08-13 15:28:53 +0800

    Finalize settings implementation

--- src/patches/settings.tsx
+++ src/patches/settings.tsx
@@ -1,9 +1,20 @@
+// Good luck reading this!
 import Patcher from "@api/Patcher";
-import { filters, findByPropsLazy, waitForModule } from "@metro";
+import { filters, waitForModule } from "@metro";
 import { Forms, I18n, NavigationNative, TabsNavigationRef } from "@metro/common";
 import { assets, findInReactTree, lazyNavigate } from "@utils";
 
 const patcher = new Patcher("settings-patcher");
+
+// @ts-expect-error
+export const sections = window.__pyoncord_sections_patch = {
+    [`Pyoncord (${__PYONCORD_COMMIT_HASH__}) ${__PYONCORD_DEV__ ? "(DEV)" : ""}`.trimEnd()]: [
+        ["PYONCORD", "Pyoncord", () => import("@ui/screens/General"), "Discord"],
+        ["PYONCORD_PLUGINS", "Plugins", () => import("@ui/screens/Plugins"), "ic_progress_wrench_24px"]
+    ]
+} as {
+    [key: string]: [key: string, title: string, getRender: () => Promise<any>, icon: string][];
+};
 
 const CustomPageRenderer = React.memo(() => {
     const navigation = NavigationNative.useNavigation();
@@ -11,9 +22,7 @@ const CustomPageRenderer = React.memo(() => {
 
     const { render: PageComponent, ...args } = route.params;
 
-    React.useEffect(() => {
-        navigation.setOptions({ ...args });
-    }, []);
+    React.useEffect(() => void navigation.setOptions({ ...args }), []);
 
     return <PageComponent />;
 });
@@ -23,24 +32,21 @@ function SettingsSection() {
     const { FormSection, FormRow, FormIcon } = Forms;
 
     const navigation = NavigationNative.useNavigation();
-    const title = `Pyoncord (${__PYONCORD_COMMIT_HASH__}) ${__PYONCORD_DEV__ ? "(DEV)" : ""}`.trimEnd();
 
-    return (
-        <FormSection key="Pyoncord" title={title}>
-            <FormRow
-                label="Pyoncord"
-                leading={<FormIcon source={assets.getAssetIDByName("Discord")} />}
-                trailing={FormRow.Arrow}
-                onPress={() => lazyNavigate(navigation, import("@ui/screens/General"), "Pyoncord")}
-            />
-            <FormRow
-                label="Plugins"
-                leading={<FormIcon source={assets.getAssetIDByName("ic_progress_wrench_24px")} />}
-                trailing={FormRow.Arrow}
-                onPress={() => lazyNavigate(navigation, import("@ui/screens/Plugins"), "Plugins")}
-            />
-        </FormSection>
-    );
+    return <>
+        {Object.keys(sections).map(sect => (
+            <FormSection key={sect} title={sect}>
+                {sections[sect].map(row => (
+                    <FormRow
+                        label={row[1]}
+                        leading={<FormIcon source={assets.getAssetIDByName(row[3])} />}
+                        trailing={FormRow.Arrow}
+                        onPress={() => lazyNavigate(navigation, row[2](), row[1])}
+                    />
+                ))}
+            </FormSection>
+        ))}
+    </>;
 }
 
 function patchPanelUI() {
@@ -57,31 +63,20 @@ function patchPanelUI() {
         const UserSettingsOverview = findInReactTree(ret.props.children, n => n.type?.name === "UserSettingsOverview");
 
         patcher.after(UserSettingsOverview.type.prototype, "renderSupportAndAcknowledgements", (_args, { props: { children } }) => {
-            try {
-                const index = children.findIndex((c: any) => c?.type?.name === "UploadLogsButton");
-                if (index !== -1) children.splice(index, 1);
-            } catch {
-                // Ignore, this is not a big deal
-            }
+            const index = children.findIndex((c: any) => c?.type?.name === "UploadLogsButton");
+            if (index !== -1) children.splice(index, 1);
         });
 
         patcher.after(UserSettingsOverview.type.prototype, "render", (_args, res) => {
-            try {
-                const titles = [I18n.Messages.BILLING_SETTINGS, I18n.Messages.PREMIUM_SETTINGS];
+            const titles = [I18n.Messages.BILLING_SETTINGS, I18n.Messages.PREMIUM_SETTINGS];
 
-                const sections = findInReactTree(
-                    res.props.children,
-                    n => n?.children?.[1]?.type === Forms.FormSection
-                ).children;
+            const sections = findInReactTree(
+                res.props.children,
+                n => n?.children?.[1]?.type === Forms.FormSection
+            ).children;
 
-                const index = sections.findIndex((c: any) => titles.includes(c?.props.label));
-                sections.splice(-~index || 4, 0, <SettingsSection />);
-            } catch (e: any) {
-                console.error(
-                    "An error occurred while trying to append Pyoncord's settings section. " +
-                    e?.stack ?? e
-                );
-            }
+            const index = sections.findIndex((c: any) => titles.includes(c?.props.label));
+            sections.splice(-~index || 4, 0, <SettingsSection />);
         });
 
         unpatch();
@@ -89,11 +84,6 @@ function patchPanelUI() {
 }
 
 function patchTabsUI() {
-    const pyonSection = [
-        ["PYONCORD", "Pyoncord", () => import("@ui/screens/General"), "Discord"],
-        ["PYONCORD_PLUGINS", "Plugins", () => import("@ui/screens/Plugins"), "ic_progress_wrench_24px"]
-    ] as [key: string, title: string, getRender: () => Promise<any>, icon: string][];
-
     waitForModule("SETTING_RENDERER_CONFIGS", module => {
         module.SETTING_RENDERER_CONFIGS.PYONCORD_CUSTOM_PAGE = {
             type: "route",
@@ -103,37 +93,47 @@ function patchTabsUI() {
             }
         };
 
-        pyonSection.forEach(([key, title, getRender, icon]) => {
-            module.SETTING_RELATIONSHIPS[key] = null;
-            module.PRESSABLE_SETTINGS_WITH_TRAILING_ARROW?.add(key);
+        for (const sect of Object.values(sections)) {
+            sect.forEach(([key, title, getRender, icon]) => {
+                module.SETTING_RELATIONSHIPS[key] = null;
+                module.PRESSABLE_SETTINGS_WITH_TRAILING_ARROW?.add(key);
 
-            module.SETTING_RENDERER_CONFIGS[key] = {
-                type: "pressable",
-                get icon() {
-                    return typeof icon === "string"
-                        ? assets.getAssetIDByName(icon)
-                        : icon;
-                },
-                onPress: () => {
-                    const ref = TabsNavigationRef.getRootNavigationRef();
-                    lazyNavigate(ref, getRender(), title);
-                }
-            };
-        });
+                module.SETTING_RENDERER_CONFIGS[key] = {
+                    type: "pressable",
+                    get icon() {
+                        return typeof icon === "string"
+                            ? assets.getAssetIDByName(icon)
+                            : icon;
+                    },
+                    onPress: () => {
+                        const ref = TabsNavigationRef.getRootNavigationRef();
+                        lazyNavigate(ref, getRender(), title);
+                    }
+                };
+            });
+        }
 
         patcher.after(module, "getSettingTitleConfig", (args, res) => {
-            pyonSection.forEach(([key, title]) => res[key] = title);
+            // This will eventually get overriden with navigation.setOptions
             res.PYONCORD_CUSTOM_PAGE = "Pyon!";
+
+            Object.values(sections).forEach(sect => {
+                sect.forEach(([key, title]) => res[key] = title);
+            });
         });
     });
 
     waitForModule("useOverviewSettings", module => {
         patcher.after(module, "useOverviewSettings", (args, res) => {
-            if (!(res instanceof Array) || res.find(sect => sect.title === "Pyoncord")) return;
+            if (!res || !Array.isArray(res)) return;
 
-            res.unshift({
-                title: "Pyoncord",
-                settings: pyonSection.map(a => a[0])
+            let index = -~res.findIndex((i: any) => i.title === I18n.Messages.ACCOUNT_SETTINGS) || 1;
+
+            Object.keys(sections).forEach(sect => {
+                res.splice(index++, 0, {
+                    title: sect,
+                    settings: sections[sect].map(a => a[0])
+                });
             });
         });
     });
